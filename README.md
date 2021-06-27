4263be80bf0277fab280abce1d5b38dce68cad11
Ruben de Groot <rpg.degroot@gmail.com>
2021-06-27 15:48:32 +0200

    Create-react-app in frontend folder

---
+++ README.md
@@ -0,0 +1,5 @@
+# Cijferhulp
+Web app om gemiddelden te berekenen en cijfers bij te houden
+
+## Ideeën
+- Grafieken om cijfers inzichtelijk te maken?
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log("Hello!");
+import express from "express";
+
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+app.listen(PORT, () => {
+    console.log("Running on port " + PORT);
+});
+
+app.use((req, res) => {
+    console.log("Visitor!");
+    res.send("Hi!");
+    res.end();
+});
