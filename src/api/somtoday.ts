8da496b2b480353a267e18bf1a06e0efa30a834d
Ruben de Groot <rpg.degroot@gmail.com>
2021-07-01 19:21:52 +0200

    Added function to get grades from somtoday account

--- src/api/somtoday.ts
+++ src/api/somtoday.ts
@@ -6,23 +6,22 @@ const getSchoolUUID = (schoolName: string | undefined) => {
         headers: {
             "Accept": "application/json"
         }
-    })
-    .then(res => res.json())
-    .then(data => {
-        for (var i = 0; i < data[0].instellingen.length; i++) {
-            const school = data[0].instellingen[i];
+    }).then(res => res.json())
+        .then(data => {
+            for (var i = 0; i < data[0].instellingen.length; i++) {
+                const school = data[0].instellingen[i];
 
-            if (school.naam == schoolName) {
-                return school.uuid;
-                break;
+                if (school.naam == schoolName) {
+                    return school.uuid;
+                    break;
+                }
             }
-        }
-    });
+        });
 }
 
-export const getAccesToken = async (schoolName: string | undefined, username: string | undefined, password: string | undefined) => {
-    const schoolUUID = await getSchoolUUID(schoolName); 
-    
+export const fetchAuthorization = async (schoolName: string | undefined, username: string | undefined, password: string | undefined): Promise<void | { accessToken: any; baseURL: any; }> => {
+    const schoolUUID = await getSchoolUUID(schoolName);
+
     const body: any = {
         "grant_type": "password",
         "username": schoolUUID + "\\" + username,
@@ -30,15 +29,15 @@ export const getAccesToken = async (schoolName: string | undefined, username: st
         "scope": "openid",
         "client_id": "D50E0C06-32D1-4B41-A137-A9A850C892C2"
     };
-    
+
     var formBody: any = [];
     for (var property in body) {
-      var encodedKey = encodeURIComponent(property);
-      var encodedValue = encodeURIComponent(body[property]);
-      formBody.push(encodedKey + "=" + encodedValue);
+        var encodedKey = encodeURIComponent(property);
+        var encodedValue = encodeURIComponent(body[property]);
+        formBody.push(encodedKey + "=" + encodedValue);
     }
     formBody = formBody.join("&");
-    
+
     return fetch("https://somtoday.nl/oauth2/token", {
         method: "POST",
         headers: {
@@ -47,8 +46,55 @@ export const getAccesToken = async (schoolName: string | undefined, username: st
         },
         body: formBody
     }).then(res => res.json())
-    .then(data => { 
-        return data.access_token
-    })
-    .catch(err => console.log(err));
+        .then(data => {
+            return { accessToken: data.access_token, baseURL: data.somtoday_api_url };
+        })
+        .catch(err => console.log(err));
+}
+
+export const getUserID = (baseURL: string, accessToken: string): Promise<number> => {
+    return fetch(baseURL + "/rest/v1/leerlingen", {
+        method: "GET",
+        headers: {
+            "Accept": "application/json",
+            "Authorization": "Bearer " + accessToken
+        }
+    }).then(res => res.json())
+        .then(data => {
+            if (data.items[0].links[0].rel === "self") {
+                return data.items[0].links[0].id;
+            } else {
+                console.log("Error while fetching userid, relation to user is not self");
+            }
+        })
+        .catch(err => console.log(err));
+}
+
+export const getGrades = async (baseURL: string, accessToken: string, userID: number): Promise<Array<Object>> => {
+    let responseLength: number = 0;
+    let totalGradeList: Array<Object> = [];
+    let rangeParamater: number = 0;
+
+    do {
+        try {
+            const res = await fetch(baseURL + "/rest/v1/resultaten/huidigVoorLeerling/" + userID, {
+                method: "GET",
+                headers: {
+                    "Accept": "application/json",
+                    "Authorization": "Bearer " + accessToken,
+                    "Range": `items=${rangeParamater}00-${rangeParamater}99`,
+                }
+            });
+            const data: any = await res.json();
+            const gradeArray: Array<Object> = data?.items;
+            totalGradeList = totalGradeList.concat(gradeArray);
+            responseLength = gradeArray.length;
+            rangeParamater++;
+        } catch (err) {
+            console.log(err);
+        }
+
+    } while (responseLength >= 99);
+
+    return totalGradeList;
 }
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
 import express from "express";
 import cors from "cors";
-import fetch, { Response } from "node-fetch";
-import { writeFileSync } from "fs";
-import { getAccesToken } from "./api/somtoday";
+import { fetchAuthorization, getGrades, getUserID } from "./api/somtoday";
 import dotenv from "dotenv";
+import { appendFile } from "fs";
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
@@ -12,14 +11,36 @@ const app = express();
 app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
 
-const accesToken = "eyJ4NXQjUzI1NiI6ImxZZ2VJNnNQMDY1Um1QTlloZ1dZcmNYcElTNWpidEM5X3V3Z0p0WV8xQVkiLCJraWQiOiJpcmlkaXVtaWRwLTYyNTQ5NDg3Mjc1MzI2NTQwNTcxNDc0NDAzNzAyMzE3NDQ4NTU3IiwiYWxnIjoiUlMyNTYifQ.eyJhdWQiOiJENTBFMEMwNi0zMkQxLTRCNDEtQTEzNy1BOUE4NTBDODkyQzIiLCJzdWIiOiI2OTAxZTg5NS1iODE4LTQ0OWQtOTEwZS04ZGNhMDE4M2NjM2FcXDczMDg1NzNiLTU1NDQtNGUwOS1iN2Q0LTY4ZmI0ZTE0NzQ5NyIsIm5iZiI6MTYyNDk5NjQ4MSwiYWF0IjoxNjI0OTk2NDgxLCJhbXIiOiJwd2QiLCJzY29wZSI6WyJvcGVuaWQiXSwiaXNzIjoiaHR0cHM6XC9cL3NvbXRvZGF5Lm5sIiwidHlwZSI6ImFjY2VzcyIsImV4cCI6MTYyNTAwMDA4MSwiaWF0IjoxNjI0OTk2NDgxLCJqdGkiOiI5ODBmYTkxNy1mNDk1LTQ5Y2EtYjNjMi0xOTQ4ZDg0YzE1ODQ6MjkyMjU3MTg4NjAwMSJ9.kk0g7Gr4YvIIs8gA0A7XezmD-OduSDdE7OpKLjCxpDtulMksPUEjrmMRUrardzIQqcyC3kOeAnOUtAm_lcSa1gYXcCMc_PgMb6vWtSsEZnTJbKw46bYR7f-BXN8YvniYoXjG7Kxz3QXe9b-w46_ALHqHCOpZg_Xp_semEvZ-tbnvj12NRoTBaNtRIBqkd6m5ktZN3WukMpHJANQ1N1fLF3FxoebW0lAanANsV3lrJ18LKWrnds7W9kl8luOpZZxzEU_ymOXZQkKHKVUStp3krzGrIvIMzF1nSic1nf-k2wNwaguek-xUZVEIAxryawAU_GvOENliuc6bHpP9yi-ogw";
-
 var formBody: any = [];
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log("Running on port " + PORT);
     // TODO: Server HTTPS maken voor veiligheid
-    getAccesToken(process.env.SCHOOL, process.env.USERNAME_SOM, process.env.PASSWORD).then(accessToken => console.log(accessToken));
+    // TODO: Error handling netjes maken
+    // TODO: Promises in somtoday code mooi maken ook met errors
+
+    const data = await fetchAuthorization(process.env.SCHOOL, process.env.USERNAME_SOM, process.env.PASSWORD);
+    const accessToken: string = await data?.accessToken;
+    const baseURL: string = await data?.baseURL;
+    const userID = await getUserID(baseURL, accessToken);
+    const grades = await getGrades(baseURL, accessToken, userID);
+    //console.log(grades);
+    let i = 0;
+    grades.forEach((grade: any) => {
+        appendFile("./res.json", JSON.stringify(grade) + ",", (err) => {
+            if (err) throw err;
+            console.log("save");
+        });
+        if (grade.type === "Toetskolom") {
+            i++;
+            console.log({
+                "Vak": grade.vak.naam,
+                "Naam": grade.omschrijving,
+                "Cijfer": grade.resultaat,
+            });
+        }
+    });
+    console.log(i);
 });
 
 app.post("/api/note", (req, res) => {
