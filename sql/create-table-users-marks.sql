c67279bcaaa1fdae2339bfa00bb81bad1607be5e
Ruben de Groot <rpg.degroot@gmail.com>
2021-07-08 21:58:43 +0200

    SQL query that creates users and marks tables

---
+++ sql/create-table-users-marks.sql
@@ -0,0 +1,27 @@
+CREATE TABLE users(
+    id                              TEXT NOT NULL PRIMARY KEY,
+    email                           VARCHAR(320) NOT NULL,
+    password                        TEXT NOT NULL,
+    name                            VARCHAR(200),
+    created_at                      TIMESTAMP NOT NULL,
+
+    UNIQUE(id, email)
+);
+
+CREATE TABLE marks(
+    id                              BIGSERIAL PRIMARY KEY,
+    mark                            NUMERIC(4, 2),
+    weighting                       INT,
+    exam_weighting                  INT,
+    type                            TEXT,
+    year                            INT,
+    period                          INT,
+    description                     TEXT,
+    subject                         TEXT,
+    subject_abbreviation            VARCHAR(10),
+    input_date                      TIMESTAMP,
+    is_examendossier_resultaat      BOOLEAN,
+    is_voortgangsdossier_resultaat  BOOLEAN,
+    origin                          VARCHAR(20),
+    user_id                         TEXT REFERENCES users(id)
+);
