df9f99bf8fe5828f1dea764b1088987074a904c4
Ruben de Groot <rpg.degroot@gmail.com>
2021-07-08 23:47:03 +0200

    Added database function for inserting a user

--- src/database.ts
+++ src/database.ts
@@ -19,7 +19,12 @@ interface Mark {
 
 const pool: Pool = new Pool();
 
+// TODO: Switch to knex.js
+// TODO: Abstract marks database queries from auth database queries
+
 export const insertMarks = async ({mark, weighting, examWeighting, type, year, period, description, subject, subjectAbbreviation, inputDate, isExamendossierResultaat, isVoortgangsdossierResultaat, origin, userId} : Mark) => {
+    // TODO: Support voor meerdere marks in een keer uploaden
+    
     try {
         const response = await pool.query("INSERT INTO marks (mark, weighting, exam_weighting, type, year, period, description, subject, subject_abbreviation, input_date, is_examendossier_resultaat, is_voortgangsdossier_resultaat, origin, user_id) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)", [mark, weighting, examWeighting, type, year, period, description, subject, subjectAbbreviation, inputDate, isExamendossierResultaat, isVoortgangsdossierResultaat, origin, userId]);
         return response;
@@ -30,7 +35,21 @@ export const insertMarks = async ({mark, weighting, examWeighting, type, year, p
 
 export const getMarks = async (userId: string) => {
     try {
+        // TODO: Check of er wel een gebruik is met user_id = user_id
+
         const response = await pool.query("SELECT * FROM marks WHERE user_id = $1", [ userId ])
+        return response;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const insertUser = async (id: string, email: string, password: string, name: string, createdAt: string) => {
+    try {
+        if (name == null || name == undefined) {
+            name = "";
+        }
+        const response = await pool.query("INSERT INTO users (id, email, password, name, created_at) VALUES($1, $2, $3, $4, $5)", [id, email, password, name, createdAt]);
         return response;
     } catch (error) {
         throw error;
