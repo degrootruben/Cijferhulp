71ec34a7ad9090300b2489ea900532cc8795497c
Ruben de Groot <rpg.degroot@gmail.com>
2021-07-12 13:17:03 +0200

    Added client-sessions package for session management

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import api from "./api";
 import dotenv from "dotenv";
+import sessions from "client-sessions";
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
@@ -9,6 +10,17 @@ const app = express();
 
 app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.json());
+app.use(sessions({
+    cookieName: "session",
+    secret: process.env.SESSION_SECRET || "SECRET",
+    duration: 90 * 60 * 1000,
+    activeDuration: 5 * 60 * 1000,
+    cookie: {
+        httpOnly: true,
+        // secure: true, // TODO: use only when https
+        ephemeral: false
+    }
+}));
 
 app.listen(PORT, async () => {
     console.log("Running on port " + PORT);
@@ -22,4 +34,8 @@ app.use("/api/", api);
 app.use((req, res) => {
     res.status(404).send({ "error": "Not found, probably using wrong route" });
     res.end();
+});
+
+process.on("exit", () => {
+    // TODO: Disconnect from database
 });
