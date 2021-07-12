3b43295c577426cb8542f19b1d6eeeca112114f8
Ruben de Groot <rpg.degroot@gmail.com>
2021-07-13 00:20:04 +0200

    Added middleware for session cookie and auth

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,13 @@ import cors from "cors";
 import api from "./api";
 import dotenv from "dotenv";
 import sessions from "client-sessions";
+import * as db from "./database";
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 const app = express();
 
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(express.json());
 app.use(sessions({
     cookieName: "session",
@@ -22,11 +23,19 @@ app.use(sessions({
     }
 }));
 
-app.listen(PORT, async () => {
-    console.log("Running on port " + PORT);
-    // TODO: Server HTTPS maken voor veiligheid
-    // TODO: Error handling netjes maken
-    // TODO: Promises in somtoday code mooi maken ook met errors
+app.use(async (req, res, next) => {
+    if (req.session && req.session.user) {
+        const emailExists = await db.emailExists(req.session.user);
+
+        if (emailExists) {
+            req.user = req.session.user;
+            req.session.user = req.session.user;
+        }
+
+        next();
+    } else {
+        next();
+    }
 });
 
 app.use("/api/", api);
@@ -34,6 +43,13 @@ app.use("/api/", api);
 app.use((req, res) => {
     res.status(404).send({ "error": "Not found, probably using wrong route" });
     res.end();
+});
+
+app.listen(PORT, async () => {
+    console.log("Running on port " + PORT);
+    // TODO: Server HTTPS maken voor veiligheid
+    // TODO: Error handling netjes maken
+    // TODO: Promises in somtoday code mooi maken ook met errors
 });
 
 process.on("exit", () => {
---
+++ src/middlewares.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from "express";
+
+export const requireLogin = (req: Request, res: Response, next: NextFunction) => {
+    if (!req.user) {
+      res.status(403).send({ "error": "You have to be logged in to access this route" });
+    } else {
+      next();
+    }
+};
