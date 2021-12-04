60b2d5a0a21cf407b19c57340a02123d27758f57
Ruben de Groot <rpg.degroot@gmail.com>
2021-12-04 18:14:45 +0100

    Updated httpStatusCodes

--- backend/src/api/httpStatusCodes.ts
+++ backend/src/api/httpStatusCodes.ts
@@ -2,6 +2,9 @@ export const status = {
     OK: 200,
     BAD_REQUEST : 400,
     NOT_FOUND : 404,
-    INTERNAL_SERVER : 500
+    UNAUTHORIZED : 401,
+    FORBIDDEN : 403,
+    INTERNAL_SERVER : 500,
+    SERVICE_UNAVAILABLE : 503,
 }
 
